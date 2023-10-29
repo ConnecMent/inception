2afdc12a1c4759869c7f357f350de7edf8adf29b
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2023-10-29 23:50:50 +0330

    Write "concat" "power2" "greet" Functions

--- mrf/main.js
+++ mrf/main.js
@@ -1,32 +1,42 @@
 // add number a to b
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 //! test add func
-console.log("add 1 to 1:", add(1,1));
+console.log("add 1 to 1:", add(1, 1));
 
 // flip a coin and log the result (head or tail)
 function flipACoin() {
-    
-    let result = Math.random();
+  let result = Math.random();
 
-    if (result >= 0.5)
-        return "HEAD";
-    
-    else
-        return "TAIL"
+  if (result >= 0.5) return "HEAD";
+  else return "TAIL";
 }
 
 //! test flipACoin func
 console.log("flipACoin:", flipACoin());
 
-
 // concat two strings
-function concat(stringA, stringB) {}
+function concat(stringA, stringB) {
+  return stringA + stringB;
+}
+
+//! Test concat func
+console.log("concat: ", concat("abc", "def"));
 
 // 2^x = ?
-function power2(x) {}
+function power2(x) {
+  let final = 1;
+  for (let i = 0; i < x; i++) {
+    final *= 2;
+  }
+
+  return final;
+}
+
+//! test power2 func
+console.log("power2 of 10:", power2(10));
 
 // user object with first name and last name, both capitalized
 // e.g. getUser('hello', 'world') is { firstName: 'Hello', lastName: 'World' }
@@ -34,7 +44,12 @@ function getUser(firstName, lastName) {}
 
 // greet someone!
 // e.g. greet('ConnecMent') is "Hello, ConnecMent! Hope you the best."
-function greet(name) {}
+function greet(name) {
+  return "Hello, " + name + " Hope you the best.";
+}
+
+//! test greet func
+console.log("Greet:", greet("Mr MRF"));
 
 // get array of n random numbers
 function getNRandomNumbers(n) {}
