29ea5caa0d714f52a40a2c23a022fcb2f02c1582
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2023-10-29 23:27:54 +0330

    Create MRF Folder :moon:

---
+++ mrf/main.js
@@ -0,0 +1,46 @@
+// add number a to b
+function add(a, b) {}
+
+// flip a coin and log the result (head or tail)
+function flipACoin() {}
+
+// concat two strings
+function concat(stringA, stringB) {}
+
+// 2^x = ?
+function power2(x) {}
+
+// user object with first name and last name, both capitalized
+// e.g. getUser('hello', 'world') is { firstName: 'Hello', lastName: 'World' }
+function getUser(firstName, lastName) {}
+
+// greet someone!
+// e.g. greet('ConnecMent') is "Hello, ConnecMent! Hope you the best."
+function greet(name) {}
+
+// get array of n random numbers
+function getNRandomNumbers(n) {}
+
+// get sorted array of n random numbers, in range min to max
+function getNRandomNumbers(n, min, max) {}
+
+// log current time with a x seconds interval, for n seconds
+function logCurrentTime(x, n) {}
+
+// higher order function: call the function in the parameters list
+// e.g. callFuncNTimes(flipACoin, 3) calls flipACoin 3 times
+function callFuncNTimes(func) {}
+
+// return true if parameter is falsy, false otherwise
+function isNil(param) {}
+
+// call property in object
+// e.g. in obj = { callableProp: function () { console.log('hello!') } }, callPropInObj(obj, 'callableProp') will log "hello!"
+function callPropInObj(object, propertyName) {}
+
+// if the email is gmail
+function isGmail(email) {}
+
+// duplicate array
+// e.g. duplicateArray([1,2,3]) is [1,2,3,1,2,3]
+function duplicateArray() {}
