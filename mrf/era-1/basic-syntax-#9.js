6f807e5c6d0371b5bd6c51c04cd9a640d95a4dca
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2023-11-07 14:55:21 +0330

    style: remove tests

--- mrf/era-1/basic-syntax-#9.js
+++ mrf/era-1/basic-syntax-#9.js
@@ -3,9 +3,6 @@ function add(a, b) {
   return a + b;
 }
 
-//! test add func
-console.assert(add(1, 1) === 2, { a: 1, b: 1, function: add.name });
-
 // flip a coin and log the result (head or tail)
 function flipACoin() {
   let result = Math.random();
@@ -14,26 +11,17 @@ function flipACoin() {
   else return "TAIL";
 }
 
-//! test flipACoin func
-console.log("flipACoin:", flipACoin());
-
 // concat two strings
 function concat(stringA, stringB) {
   // return stringA + stringB;
   return stringA.concat(stringB);
 }
 
-//! Test concat func
-console.log("concat: ", concat("abc", "def"));
-
 // 2^x = ?
 function power2(x) {
   // return Math.pow(2, x);
   return 2 ** x;
 }
-
-//! test power2 func
-console.log("power2 of 10:", power2(10));
 
 // user object with first name and last name, both capitalized
 // e.g. getUser('hello', 'world') is { firstName: 'Hello', lastName: 'World' }
@@ -44,18 +32,12 @@ function getUser(firstName, lastName) {
   return arr;
 }
 
-//! test getUser function
-console.log("getUser whami:", getUser("mmd", "mmdi"));
-
 // greet someone!
 // e.g. greet('ConnecMent') is "Hello, ConnecMent! Hope you the best."
 function greet(name) {
   // return "Hello, " + name + " Hope you the best.";
   return `Hello, ${name} Hope you the best.`;
 }
-
-//! test greet func
-console.log("Greet:", greet("Mr MRF"));
 
 // get array of n random numbers
 function getNRandomNumbers(n) {
@@ -72,9 +54,6 @@ function getNRandomNumbers(n) {
   return final.map(Math.random);
 }
 
-//! test of getNRandomNumbers
-console.log("getNRandomNumbers: 10", getNRandomNumbers(10));
-
 // get sorted array of n random numbers, in range min to max
 function getNRandomNumbersRange(n, min, max) {
   const final = new Array();
@@ -87,12 +66,6 @@ function getNRandomNumbersRange(n, min, max) {
 
   return final;
 }
-
-//! test of getNRandomNumbersRange
-console.log(
-  "getNRandomNumbersRange: 10, 5, 15",
-  getNRandomNumbersRange(10, 5, 15)
-);
 
 // log current time with a x seconds interval, for n seconds
 function logCurrentTime(x, n) {
@@ -119,9 +92,6 @@ function logCurrentTime(x, n) {
   }
 }
 
-//! test of logCurrentTime
-logCurrentTime(2, 10);
-
 // higher order function: call the function in the parameters list
 // e.g. callFuncNTimes(flipACoin, 3) calls flipACoin 3 times
 function callFuncNTimes(func, n, ...funcParam) {
@@ -130,11 +100,6 @@ function callFuncNTimes(func, n, ...funcParam) {
   }
 }
 
-//! Test callFuncNTimes
-callFuncNTimes(() => {
-  console.log(flipACoin());
-}, 3);
-
 // return true if parameter is falsy, false otherwise
 function isNil(param) {
   // return param ? false : true;
@@ -142,27 +107,11 @@ function isNil(param) {
   return !param;
 }
 
-//! test of isNil
-console.log("isNil:", isNil(0x0));
-console.log("isNil:", isNil(0));
-console.log("isNil:", isNil(0.0));
-console.log("isNil:", isNil(false));
-console.log("isNil:", isNil(""));
-console.log("isNil:", isNil("MRF"));
-
 // call property in object
 // e.g. in obj = { callableProp: function () { console.log('hello!') } }, callPropInObj(obj, 'callableProp') will log "hello!"
 function callPropInObj(object, propertyName) {
   object[propertyName]?.();
 }
-
-//! test callpropinobj
-const obj = {
-  callableProp: function () {
-    console.log("hello!");
-  },
-};
-callPropInObj(obj, "callableProp");
 
 // if the email is gmail
 function isGmail(email) {
@@ -175,16 +124,8 @@ function isGmail(email) {
   return email.endsWith("@gmail.com");
 }
 
-//! test isGmail
-console.log(isGmail("mmd.com"));
-console.log(isGmail("mmd@ggg.com"));
-console.log(isGmail("mmd@gmail.com"));
-
 // duplicate array
 // e.g. duplicateArray([1,2,3]) is [1,2,3,1,2,3]
 function duplicateArray(arr) {
   return arr.concat(arr);
 }
-
-//! test duplicateArray
-console.log(duplicateArray([1, 2, 3]));
