65348b88757f2728e6a890b9867eca0795e680b3
Kiarash <kia.iranpour1380@gmail.com>
2023-11-10 13:13:01 +0330

    i dont know why my changes didn't pushed :|

--- kiarash/TS/TS:Narrowing-Discriminated-unions/src/index.ts
+++ kiarash/TS/TS:Narrowing-Discriminated-unions/src/index.ts
@@ -1,28 +1,28 @@
 interface Base {
-    foo: string;
-    bar: string;
-  }
-  
-  interface A extends Base {
-    bazA: string;
-  }
-  
-  interface B extends Base {
-    bazB: string;
-  }
-  
-  type AOrB = A | B;
-  
-  const aOrB: AOrB = {
-    foo: 'foo',
-    bar: 'bar',
-    ...(Math.random() > 0.5 ? { bazA: 'bazA' } : { bazB: 'bazB' }),
-  }
-  
-  if (/* condition for a */) {
-    console.log('aOrB is an A', aOrB.bazA);
-  } else {
-    console.log('aOrB is a B', aOrB.bazB);
-  }
-  
-  process.exit(0);
+  foo: string;
+  bar: string;
+}
+
+interface A extends Base {
+  bazA: string;
+}
+
+interface B extends Base {
+  bazB: string;
+}
+
+type AOrB = A | B;
+
+const aOrB: AOrB = {
+  foo: "foo",
+  bar: "bar",
+  ...(Math.random() > 0.5 ? { bazA: "bazA" } : { bazB: "bazB" }),
+};
+
+if ("bazA" in aOrB) {
+  console.log("aOrB is an A", aOrB.bazA);
+} else {
+  console.log("aOrB is a B", aOrB.bazB);
+}
+
+process.exit(0);
