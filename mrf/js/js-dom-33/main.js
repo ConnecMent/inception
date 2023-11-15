7c88fdde7e067904a58ad6d7f4c018a241a9dfc7
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2023-11-15 20:11:54 +0330

    feat: update main.js
    - e is removed
    - the c subtree is moved to g. (a -> b -> g -> c -> d)
    - i and j elements are added as a child to f and h, respectively, considering size constraints (50% of parent)

--- mrf/js/js-dom-33/main.js
+++ mrf/js/js-dom-33/main.js
@@ -5,7 +5,7 @@ function getRandomNumbersRange(min, max) {
 function ButtonClick() {
   const divs = document.getElementsByTagName("div");
   const divName = ["a", "b", "c", "d", "e", "g", "f", "h"];
-  const allDivs = {};
+  const allDivs = new Object();
 
   //! 1
   for (let i = 0; i < divs.length; i++) {
@@ -28,7 +28,7 @@ function ButtonClick() {
   // const aSubTree = document.body.childNodes[1].getElementsByTagName("div");
   // or
   const aSubTree = allDivs["a"].getElementsByTagName("div");
-  let indexA = getRandomNumbersRange(0, aSubTree.length);
+  let indexA = getRandomNumbersRange(0, aSubTree.length - 1);
   aSubTree[indexA].style.backgroundColor = "yellow";
 
   let blueColor = getRandomNumbersRange(50, 255);
@@ -36,4 +36,22 @@ function ButtonClick() {
   let fColor = `rgba(0, 0, ${blueColor}, ${AlphaColor})`;
 
   allDivs["f"].style.backgroundColor = fColor;
+
+  //! 4
+  allDivs["b"].removeChild(allDivs["e"]);
+
+  //! 5
+  allDivs["g"].appendChild(allDivs["c"]);
+
+  //! 6
+  let iText = document.createTextNode("i");
+  const newIDiv = document.createElement("div");
+  newIDiv.appendChild(iText);
+
+  let jText = document.createTextNode("j");
+  const newJDiv = document.createElement("div");
+  newJDiv.appendChild(jText);
+
+  allDivs["f"].appendChild(newIDiv);
+  allDivs["h"].appendChild(newJDiv);
 }
