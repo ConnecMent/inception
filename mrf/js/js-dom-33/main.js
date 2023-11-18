6b395e8330d76f6d87bfebe6e0b073ef75ae39a4
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2023-11-18 22:20:12 +0330

    fix yellow & blue random color

--- mrf/js/js-dom-33/main.js
+++ mrf/js/js-dom-33/main.js
@@ -2,6 +2,12 @@ function getRandomNumbersRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function generateHSLRandomColor(h) {
+  const s = getRandomNumbersRange(0, 100);
+  const l = getRandomNumbersRange(0, 100);
+  return "hsl(" + h + ", " + s + "%, " + l + "%)";
+}
+
 function handleButtonClick() {
   const divs = document.getElementsByTagName("div");
   const divName = ["a", "b", "c", "d", "e", "g", "f", "h"];
@@ -18,12 +24,12 @@ function handleButtonClick() {
   allDivs["f"].style.height = newWidth + "px";
 
   const aSubTree = allDivs["a"].getElementsByTagName("div");
-  let indexA = getRandomNumbersRange(0, aSubTree.length - 1);
-  aSubTree[indexA].style.backgroundColor = "yellow";
 
-  let blueColor = getRandomNumbersRange(50, 255);
-  let AlphaColor = getRandomNumbersRange(50, 100) / 100;
-  let fColor = `rgba(0, 0, ${blueColor}, ${AlphaColor})`;
+  for (let i = 0; i < aSubTree.length - 1; i++) {
+    aSubTree[i].style.backgroundColor = generateHSLRandomColor(60);
+  }
+
+  let fColor = generateHSLRandomColor(240);
 
   allDivs["f"].style.backgroundColor = fColor;
 
