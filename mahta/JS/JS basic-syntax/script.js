c2777ee40e6fbdd7ca6370e81151221b30312364
MahtaMirzaei <mahtamirzaei1383@gmail.com>
2023-11-30 11:08:48 +0330

    first version of completed Js's basic syntax

--- mahta/JS/JS basic-syntax/script.js
+++ mahta/JS/JS basic-syntax/script.js
@@ -24,3 +24,77 @@ function getUser(firstName, lastName) {
     const LastName=lastName.replace(lastName.charAt(0),lastName.charAt(0).toUpperCase());
     return{FirstName, LastName};
 }
+
+function greet(name) {
+    console.log(`Hi ${name}, nice to meet you!`)
+}
+
+function getNRandomNumbers(n) {
+    let random_numbers=[];
+    for (i = 0; i < n; i++){
+        random_numbers.push(Math.random()*1000);
+    }
+    return random_numbers;
+}
+
+function getNRandomNumbers(n, min, max) {
+    let random_numbers=[];
+    for (i = 0; i < n; i++){
+        random_numbers.push(Math.random()*(max-min)+min);
+    }
+    return random_numbers;   
+}
+
+//Not functioning
+function logCurrentTime(x, n) {
+    for (let i = 0; i <= Math.floor(n/x); i++) {
+      setTimeout(() => { console.log('Time:'+ Date()); }, i * x * 1000);
+    }
+  }
+  
+  
+
+function callFuncNTimes(flipACoin) {
+    let count = 0;
+    for(let i = 0; i < 3; i++){
+        if (flipACoin()==='head'){
+            count++;
+        }
+    }
+    if (count <= 2){
+        console.log("heads won!");
+    }
+    else {
+        console.log("tails won!");
+    }
+}
+
+  //couldn't undrestand what we need
+function isNil(param) {
+    return !param;
+  }
+
+  //couldn't undrestand what we need
+  function callPropInObj(object, propertyName) {
+    object[propertyName]();
+  }
+  
+
+function isGmail(email) {
+    const searchTerm = "@Gmail.com";
+    if (email.includes(searchTerm)) {
+        console.log("Is Gmail!");
+      } else {
+        console.log("Is not Gmail!");
+      }
+}
+
+function duplicateArray() {
+    let fruits = ['Apple', 'Orange', 'Strawberry'];
+    let duplicatedFruits = fruits;
+    for (let x in fruits) {
+      duplicatedFruits.push(fruits[x]);
+    }
+    console.log(duplicatedFruits);
+  }
+ 
