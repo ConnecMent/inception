37a4d05dde7aade4ac40c2ca081de269f109f2e6
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2023-12-03 20:12:24 +0330

    feat: added `addCard` Function

---
+++ mrf/js/56-data-fetching/mian.js
@@ -0,0 +1,34 @@
+function addCard(titleIn, textIn, authorIn) {
+    const master = document.createElement("div");
+    master.setAttribute("class", "card");
+  
+    const title = document.createElement("p");
+    title.textContent = titleIn;
+  
+    const text = document.createElement("p");
+    text.textContent = textIn;
+  
+    const author = document.createElement("p");
+    author.textContent = authorIn;
+  
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "delete");
+    deleteButton.textContent = "Delete";
+  
+    const updateButton = document.createElement("button");
+    updateButton.setAttribute("class", "update");
+    updateButton.textContent = "Update";
+  
+    master.appendChild(title);
+    master.appendChild(text);
+    master.appendChild(author);
+    master.appendChild(deleteButton);
+    master.appendChild(updateButton);
+  
+    return master;
+  }
+  
+  for (let i = 0; i < 10; i++) {
+    document.body.appendChild(addCard("test", "test2", "test3"));
+  }
+  
