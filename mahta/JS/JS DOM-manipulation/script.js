ddf89a9aa40aec22584d05c0f7a47e10032bf347
MahtaMirzaei <mahtamirzaei1383@gmail.com>
2023-12-07 12:25:08 +0330

    in the middle of trying to create elements in the page

--- mahta/JS/JS DOM-manipulation/script.js
+++ mahta/JS/JS DOM-manipulation/script.js
@@ -1,28 +1,43 @@
+
+const button = document.getElementById('reset');
+const button2 = document.getElementById('set');
 function onButtonClickSet() {
-    alert('Set clicked!');
-    const button1 = document.getElementById('set');
-    button1.style.display = 'none';
-    const button2 = document.getElementById('reset');
-    button2.style.display = 'block';
+    button2.style.display = 'none';
+    button.style.display = 'block';
   }
-  
-  function onButtonClickReset() {
-      alert('Reset clicked!');
-      button1.style.display = 'none';
-      const button2 = document.getElementById('set');
-      button2.style.display = 'block';
-    const button1 = document.getElementById('reset');
-  }
-  
-  function hideButton() {
-    var button = document.getElementById('reset');
+
+  const box= [];  
+ function firstPage() {
+
     button.style.display = 'none';
+    button2.style.display = 'block';
+ 
+    const elm = ["a", "b", "c", "d", "e", "g", "f", "h"];
+    box.forEach((index) => {
+      elm[index] = document.createElement("div");
+    });
+    
+
+    document.body.appendChild(box[0]);
+    box[0].appendChild(box[1]);
+    box[1].appendChild(box[2]);
+    box[2].appendChild(box[3]);
+    box[1].appendChild(box[4]);
+    box[1].appendChild(box[5]);
+    document.body.appendChild(box[6]);
+    box[6].appendChild(box[7]);
+
+    
+    box[0].setAttribute("class", "container");
+    box[6].setAttribute("class", "container");
+  
   }
 
-  window.onload = hideButton;
-
+  
   const set = document.querySelector('#set');
   set.addEventListener('click', onButtonClickSet);
   
   const reset = document.querySelector('#reset');
-  reset.addEventListener('click', onButtonClickReset);
+  reset.addEventListener('click', firstPage);
+  
+  window.onload = firstPage;
