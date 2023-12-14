1c30a63a023a1138299444ef86065785c905820a
Mohammad Kazemi <mohamad.kazemi1383@gmail.com>
2023-12-14 13:54:42 +0330

    change the implementation considering the points that kiarash said

--- mohammad/JS/JS-simpleDataFetching/main.js
+++ mohammad/JS/JS-simpleDataFetching/main.js
@@ -1,11 +1,21 @@
 const titleArr = new Array();
 const bodyArr = new Array();
+const promises = new Array();
 for (let i = 1; i <= 10; i++) {
-  fetch(`https://jsonplaceholder.typicode.com/posts/${i}`)
-    .then((response) => response.json())
-    .then((post) => {
+  promises.push(
+    fetch(`https://jsonplaceholder.typicode.com/posts/${i}`).then((response) =>
+      response.json()
+    )
+  );
+}
+const myPromise = Promise.all(promises);
+myPromise
+  .then((posts) => {
+    posts.forEach((post) => {
       titleArr.push(post["title"]);
       bodyArr.push(post["body"]);
-      if (i == 10) console.table([titleArr, bodyArr]);
     });
-}
+
+    console.table([titleArr, bodyArr]);
+  })
+  .catch((error) => console.error("Error fetching posts:", error));
