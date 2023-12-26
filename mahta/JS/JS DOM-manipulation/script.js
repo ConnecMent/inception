a2b1457c6fa0d093eeb8a190ddb75b5e095bae06
MahtaMirzaei <mahtamirzaei1383@gmail.com>
2023-12-26 15:33:48 +0330

    not mush really

--- mahta/JS/JS DOM-manipulation/script.js
+++ mahta/JS/JS DOM-manipulation/script.js
@@ -4,6 +4,8 @@ function generateRandomColor(y, b) {
   return `rgb(${yellow}, ${yellow}, ${blue})`;
 }
 
+
+
 function onButtonClickSet() {
   document.body.innerHTML = "";
   const buttonTag = document.createElement("button");
@@ -11,30 +13,16 @@ function onButtonClickSet() {
   buttonTag.addEventListener("click", firstPage);
   document.body.appendChild(buttonTag);
 
-  const elm = {
-    a: 0,
-    b: 1,
-    c: 2,
-    d: 3,
-    f: 4,
-    g: 5,
-    h: 6,
-    i: 7,
-    j: 8,
-  };
-
-  const divElements = {};
-
-  
+  const elm = { a: 0, b: 1, c: 2, d: 3, f: 4, g: 5, h: 6, i: 7, j: 8 };
+  const divElements = [];
   Object.keys(elm).forEach((element) => {
-    divElements[element] = document.createElement("div");
+    const div = document.createElement("div");
+    div.innerText = element;
+    divElements.push(div);
   });
-
 
   divElements["f"].style.width = "250px";
   divElements["f"].style.height = "250px";
-
-
 
   const container = document.createElement("div");
 
@@ -60,32 +48,25 @@ function onButtonClickSet() {
   const subTree = Object.values(elm).map((x) => divElements[x]);
 
   subTree.forEach((x) => {
-    if ((Array.from(divElements["f"].childNodes).includes(x))||(Array.from(divElements[6].childNodes).includes(x)) ){
+    if (
+      Array.from(divElements["f"].childNodes).includes(x) ||
+      Array.from(divElements["f"].childNodes).includes(x)
+    ) {
       x.style.backgroundColor = generateRandomColor(0, 255);
     } else {
       x.style.backgroundColor = generateRandomColor(255, 0);
     }
   });
-  divElements["f"].style.backgroundColor=generateRandomColor(0, 255)
+  divElements["f"].style.backgroundColor = generateRandomColor(0, 255);
 }
 
-function firstPage() {
-  document.body.innerHTML = "";
+function firstState() {
   const buttonTag = document.createElement("button");
   buttonTag.textContent = "Click Me!";
   buttonTag.addEventListener("click", onButtonClickSet);
 
   document.body.appendChild(buttonTag);
-  const elm = {
-    a: 0,
-    b: 1,
-    c: 2,
-    d: 3,
-    e: 4,
-    f: 5,
-    g: 6,
-    h: 7,
-  };
+  const elm = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7 };
 
   Object.keys(elm).forEach((element) => {
     elm[element] = document.createElement("div");
@@ -108,6 +89,11 @@ function firstPage() {
   elm["f"].classList.add("container");
   elm["g"].classList.add("container");
   elm["h"].classList.add("container");
+}
+
+function firstPage() {
+  document.body.innerHTML = "";
+  firstState();
 }
 
 const reset = document.querySelector("#reset");
