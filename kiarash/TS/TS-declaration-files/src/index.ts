083d79d852526972a3d1fd6ed7bd6db956f5cf3d
Kiarash <kia.iranpour1380@gmail.com>
2023-12-28 18:35:53 +0330

    declaration files task done

---
+++ kiarash/TS/TS-declaration-files/src/index.ts
@@ -0,0 +1,7 @@
+import obj, { foo, bar } from "./myModule.js";
+
+console.log(obj);
+console.log(foo());
+console.log(bar);
+
+export {};
---
+++ kiarash/TS/TS-declaration-files/src/myModule.d.ts
@@ -0,0 +1,8 @@
+export function foo(): "hello" | 5;
+export const bar: "hello worldd";
+export function returnObject(): void;
+declare namespace _default {
+  let foo: string;
+  let bar: string;
+}
+export default _default;
